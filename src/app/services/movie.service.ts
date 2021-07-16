e870bba3b36b01a5cd3e99d72a14cd517119f1ce
Fernando Bondioni <fernandobondioni@gmail.com>
2021-07-16 17:44:11 -0300

    movie service done

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movies } from '../models/movies';
 
 const enum endpoint {
   latest = '/movie/latest',
   now_playing = '/movie/now_playing',
   top_rated = '/movie/top_rated',
+  popular = '/movie/popular',
   upcoming = '/movie/upcoming',
   trending = '/trending/all/week',
   originals = '/discover/tv',
@@ -15,6 +19,50 @@ const enum endpoint {
 })
 export class MovieService {
   private URL = 'http://api.themoviedb.org/3';
+  private api_key = environment.api;
 
   constructor(private http: HttpClient) {}
+
+  getLatestMovie(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.latest}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
+  getNowPlaying(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.now_playing}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
+  getTopRated(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.top_rated}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
+  getPopular(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.popular}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
+  getTrending(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.trending}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
+  getOriginals(): Observable<Movies> {
+    return this.http.get<Movies>(`${this.URL}${endpoint.originals}`, {
+      params: {
+        api_key: this.api_key,
+      },
+    });
+  }
 }
